dce376ea8a7f70d288a99ce69345fd1381b6d745
youssef imlyhen <71871551+youssef-imlyhen@users.noreply.github.com>
2022-07-02 17:08:35 +0100

    Add files via upload

--- client/index.js
+++ client/index.js
@@ -18,63 +18,54 @@ async function getWords() {
     const url = "http://localhost:8080/words/fr_en/"
     try {
         const words = await fetch(url);
-        localStorage.setItem("words", JSON.stringify(words.json())); 
+        return await words.json()
     } catch (error) {
-        console.log(error);
     }
 }
 
 async function getPhrases() {
     const url = "http://localhost:8080/phrases/en_fr/"
     try {
-        const phrases = await fetch(url);
-        localStorage.setItem("phrases", phrases.json())
+        const words = await fetch(url);
+        return await words.json()
     } catch (error) {
-        console.log(error);
     }
 }
 
-async function renderWords(startI) {
-        var words = localStorage.getItem("words");  
-        if (!words)    await getWords();
-        words = JSON.parse(words)
-        
+async function renderWords() {
+    let words = await getWords();
+        console.log(words);
         textHolders.forEach((text, i) => {
-        text.innerText = words[i + startI][0]
+        text.innerText = words[i][0]
     })
     firstTitle.innerText = "French"
     scndTitle.innerText = "English"
+    localStorage.setItem("words", JSON.stringify(words));
+    
 }
-async function renderPhrases(startI) {
-    var phrases = localStorage.getItem("phrases");  
-        if (!phrases)    {
-            await getPhrases();
-            console.log('went from here');
-            
-            var phrases = localStorage.getItem("phrases");  
-        }
-        phrases = JSON.parse(phrases)
-        console.log("rendrPHrases");
+async function renderPhrases() {
+    let phrases = await getPhrases();
+        console.log(phrases);
         textHolders.forEach((text, i) => {
-        text.innerText = phrases[(i + startI)][0]
+        text.innerText = phrases[i][0]
     })
     firstTitle.innerText = "English"
     scndTitle.innerText = "French"
+    localStorage.setItem("phrases", JSON.stringify(phrases))
 }
 
 
 verifyBtn.addEventListener('click', e => {
     e.preventDefault();
     if (firstTitle.innerText === "English") 
-        var rightAnswers = localStorage.getItem("phrases");  
+        var rightAsnwers = localStorage.getItem("phrases");  
         
     else
-        var rightAnswers = localStorage.getItem("words");  
+        var rightAsnwers = localStorage.getItem("words");  
 
-    rightAnswers = JSON.parse(rightAnswers)
-    answers.forEach((answer, i) => { 
-        console.log(rightAnswers[i][1]);
-        if(answer.value.toLowerCase().trim() !== rightAnswers[i][1].toLowerCase().trim()){
+    rightAsnwers = JSON.parse(rightAsnwers)
+    answers.forEach((answer, i) => {
+        if(answer.value !== rightAsnwers[i][1]){
             feedbacks[i].innerText = "wrong"
             feedbacks[i].style.color = 'red'
         }
@@ -90,22 +81,36 @@ togleAnswers.addEventListener("click",e => {
     e.preventDefault();
     isClicked = !isClicked;
     if (isClicked) {
-        updateAnswers()  
+        if (firstTitle.innerText === "English") 
+            var rightAsnwers = localStorage.getItem("phrases");  
+        
+        else
+            var rightAsnwers = localStorage.getItem("words");  
+
+        rightAsnwers = JSON.parse(rightAsnwers)
+        feedbacks.forEach((feedback, i) => {
+            feedback.innerText = rightAsnwers[i][1]
+            feedback.style.color = 'balck'
+        })    
     }
     else{
-        hideAnswers()
+        feedbacks.forEach((feedback, i) => {
+            feedback.innerText = "feedback"+(i+1);
+            feedback.style.color = 'balck'
+
+        })
     }
 })
 wordsFrtoEnbtn.addEventListener("click", e => {
     mainMenu.style.display = "none";
     hero.style.display = "block";
-    renderWords(0)
+    renderWords()
 })
 
 phFrToEnBtn.addEventListener("click", e => {
     mainMenu.style.display = "none";
     hero.style.display = "block";
-    renderPhrases(0)
+    renderPhrases()
 })
 
 
@@ -113,117 +118,4 @@ mainBtn.addEventListener("click", e => {
     e.preventDefault();
     hero.style.display = "none";
     mainMenu.style.display = "block";
-    pageNum.innerText = "Page "+ (1) + "/4"
-
 })
-
-
-const pageNum = document.querySelector('.pagination');
-const MAX_PAGE_NUM = 4;
-
-nextBtn.addEventListener("click", e => {
-    e.preventDefault()
-    hideAnswers()
-    let curPageNum = pageNum.innerText[5];
-    if (firstTitle.innerText === "English"){
-        switch (curPageNum) {
-            case "1":
-                pageNum.innerText = "Page "+ (+curPageNum + 1) + "/4"
-                renderPhrases(5)
-                break;
-            case "2":
-                pageNum.innerText = "Page "+ (+curPageNum + 1) + "/4"
-                renderPhrases(10)
-                break;
-            case "3":
-                pageNum.innerText = "Page "+ (+curPageNum + 1) + "/4"
-                renderPhrases(15)
-                break;
-            case "4":
-                pageNum.innerText = "Page "+ (1) + "/4"
-                renderPhrases(0)
-                break;
-        
-            default:
-                break;
-        }
-    }else{
-        switch (curPageNum) {
-            case "1":
-                pageNum.innerText = "Page "+ (+curPageNum + 1) + "/4"
-                renderWords(5)
-                break;
-            case "2":
-                pageNum.innerText = "Page "+ (+curPageNum + 1) + "/4"
-                renderWords(10)
-                break;
-            case "3":
-                pageNum.innerText = "Page "+ (+curPageNum + 1) + "/4"
-                renderWords(15)
-                break;
-            case "4":
-                pageNum.innerText = "Page "+ (1) + "/4"
-                renderWords(0)
-                break;
-        
-            default:
-                break;
-    }
-
-}})
-
-function renderAnswers(rightAnswers, startI) {
-    rightAnswers = JSON.parse(rightAnswers)
-        feedbacks.forEach((feedback, i) => {
-            feedback.style.color = 'black'
-            feedback.innerText = rightAnswers[i + startI][1]
-        })   
-} 
-
-function updateAnswers(rightAnswers) {
-    if (firstTitle.innerText === "English")
-        var rightAnswers = localStorage.getItem("phrases");  
-    else
-        var rightAnswers = localStorage.getItem("words");
-          
-        let curPageNum = pageNum.innerText[5];
-            switch (curPageNum) {
-                case "1":
-                    renderAnswers(rightAnswers, 0)
-                    break;
-                case "2":
-                    renderAnswers(rightAnswers, 5)
-                    break;
-                case "3":
-                    renderAnswers(rightAnswers, 10)
-                    break;
-                case "4":
-                    renderAnswers(rightAnswers, 15)
-                    break;
-            
-                default:
-                    break;
-            }
-}
-function hideAnswers() {
-    feedbacks.forEach((feedback, i) => {
-        feedback.style.color = 'black'
-        feedback.innerText = "";
-
-    })
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
